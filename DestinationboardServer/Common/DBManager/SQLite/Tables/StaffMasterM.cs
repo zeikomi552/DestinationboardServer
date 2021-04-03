53c77678482760a04d5d110bfec0a91379ce271a
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-04-04 07:49:00 +0900

    スケルトン関数を作成

---
+++ DestinationboardServer/Common/DBManager/SQLite/Tables/StaffMasterM.cs
@@ -0,0 +1,13 @@
+﻿using DestinationboardServer.Common.DBManager.SQLite.TablesBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinationboardServer.Common.DBManager.SQLite.Tables
+{
+    public class StaffMasterM : StaffMasterBase
+    {
+    }
+}
