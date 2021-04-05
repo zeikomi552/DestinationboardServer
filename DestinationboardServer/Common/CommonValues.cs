f8de020b576d5e890344a91a8b85c5552edc140a
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-04-06 05:42:19 +0900

    受信処理の作成

---
+++ DestinationboardServer/Common/CommonValues.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinationboardServer.Common
+{
+	public class CommonValues 
+	{
+		static CommonValues _Instance = new CommonValues();
+
+		private CommonValues()
+		{
+
+		}
+
+		public static CommonValues GetInstance()
+		{
+			return _Instance;
+
+		}
+
+		#region サーバー名[ServerName]プロパティ
+		/// <summary>
+		/// サーバー名[ServerName]プロパティ用変数
+		/// </summary>
+		string _ServerName = "127.0.0.1";
+		/// <summary>
+		/// サーバー名[ServerName]プロパティ
+		/// </summary>
+		public string ServerName
+		{
+			get
+			{
+				return _ServerName;
+			}
+			set
+			{
+				if (!_ServerName.Equals(value))
+				{
+					_ServerName = value;
+					//NotifyPropertyChanged("ServerName");
+				}
+			}
+		}
+		#endregion
+		#region ポート番号[Port]プロパティ
+		/// <summary>
+		/// ポート番号[Port]プロパティ用変数
+		/// </summary>
+		int _Port = 552;
+		/// <summary>
+		/// ポート番号[Port]プロパティ
+		/// </summary>
+		public int Port
+		{
+			get
+			{
+				return _Port;
+			}
+			set
+			{
+				if (!_Port.Equals(value))
+				{
+					_Port = value;
+					//NotifyPropertyChanged("Port");
+				}
+			}
+		}
+		#endregion
+
+
+	}
+}
--- DestinationboardServer/Program.cs
+++ DestinationboardServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DestinationboardCommunicationLibrary.Communication;
+using DestinationboardServer.Common;
 using DestinationboardServer.Common.DBManager.SQLite.Tables;
 using DestinationboardServer.Common.DBManager.SQLite.TablesBase;
 using System;
@@ -12,9 +13,11 @@ namespace DestinationboardServer
         static void Main(string[] args)
         {
             DestinationbardCommunicationAPIService service 
-                = new DestinationbardCommunicationAPIService("127.0.0.1", 552);
+                = new DestinationbardCommunicationAPIService(CommonValues.GetInstance().ServerName, CommonValues.GetInstance().Port);
 
             service.RecieveRegistStaffEvent += Service_RecieveRegstStaffEvent;
+            service.RecieveGetRegistStaffEvent += Service_RecieveGetRegistStaffEvent;
+
 
 
             service.Listen();
@@ -23,6 +26,36 @@ namespace DestinationboardServer
 
 
         }
+
+        private static void Service_RecieveGetRegistStaffEvent(object sender, EventArgs e)
+        {
+            GetStaffsRequest request = ((gRPCArgsRcv)e).Request as GetStaffsRequest;
+            GetStaffsReply reply = ((gRPCArgsRcv)e).Replay as GetStaffsReply;
+
+            try
+            {
+
+                var list = StaffMasterM.Select();
+                
+                foreach (var item in list)
+                {
+                    StaffMasterReply tmp = new StaffMasterReply();
+                    tmp.StaffID = item.StaffID;
+                    tmp.StaffName = item.StaffName;
+                    tmp.CreateDate = item.CreateDate.ToString("yyyy/MM/dd");
+                    tmp.CreateUser = item.CreateUser;
+                    tmp.Display = item.Display;
+                    reply.StaffInfoList.Add(tmp);
+                }
+
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+
 
         private static void Service_RecieveRegstStaffEvent(object sender, EventArgs e)
         {
@@ -36,7 +69,7 @@ namespace DestinationboardServer
                 StaffMasterM.UpdateList(request);
 
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 reply.ErrorCode = 2;
 
