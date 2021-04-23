8a791c7acb81ac12a6c1b3629057d8017e39ad00
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-04-23 12:51:17 +0900

    SQLiteファイルが存在しない場合のメッセージを追加

--- DestinationboardServer/Program.cs
+++ DestinationboardServer/Program.cs
@@ -5,6 +5,7 @@ using DestinationboardServer.Common.DBManager.SQLite;
 using DestinationboardServer.Common.DBManager.SQLite.Tables;
 using DestinationboardServer.Common.DBManager.SQLite.TablesBase;
 using System;
+using System.IO;
 
 namespace DestinationboardServer
 {
@@ -31,12 +32,21 @@ namespace DestinationboardServer
 
                 // SQLite用のファイルパス
                 SQLiteDataContext.SQLitePath = config.SQLitePath;
-                // サービスの作成
-                Listener listener = new Listener(config.HostName, config.Port);
-
                 // ログ出力
                 Console.WriteLine(string.Format("SQLitePath={0}", config.SQLitePath));
                 Console.WriteLine(string.Format("Port={0}", config.Port));
+
+                if (!File.Exists(config.SQLitePath))
+                {
+                    Console.WriteLine(string.Format("SQLiteファイルが見つかりません。"));
+                    Console.WriteLine(string.Format(@"Config\DestinationServer.confの設定を確認してください。"));
+                    Console.ReadLine();
+                    return;
+                }
+
+                // サービスの作成
+                Listener listener = new Listener(config.HostName, config.Port);
+
 
                 // サービスのスタート
                 listener.Start();
