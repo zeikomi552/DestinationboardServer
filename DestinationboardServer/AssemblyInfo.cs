e0f5fab1f2ae6dfb3f3b53d2b66b0087de687c9e
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-04-23 12:32:42 +0900

    log4netの設定

---
+++ DestinationboardServer/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+[assembly: log4net.Config.XmlConfigurator(Watch = true, ConfigFile = "log4net.config")]
+
+// このような SDK スタイルのプロジェクトの場合、以前はこのファイルで定義していたいくつかのアセンブリ属性がビルド時に自動的に追加されて、プロジェクトのプロパティで定義されている値がそれに設定されるようになりました。組み込まれる属性と、このプロセスをカスタマイズする方法の詳細については、次を参照してください:
+// https://aka.ms/assembly-info-properties
+
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから参照できなくなります。このアセンブリ内の型に COM からアクセスする必要がある場合は、その型の
+// ComVisible 属性を true に設定してください。
+
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります。
+
+[assembly: Guid("4ec7a353-b1b6-4eac-aeae-f6dd6eec9de5")]
--- DestinationboardServer/Program.cs
+++ DestinationboardServer/Program.cs
@@ -10,26 +10,45 @@ namespace DestinationboardServer
 {
     class Program
     {
+        #region ロガー
+        /// <summary>
+        /// ロガー
+        /// </summary>
+        protected static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        #endregion
         /// <summary>
         /// メイン関数
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            ConfigManager config = new ConfigManager();
-            config.LoadConfig();
+            _logger.Error("test");
+            try
+            {
+                // Configの取得処理
+                ConfigManager config = new ConfigManager();
+                config.LoadConfig();
 
-            // SQLite用のファイルパス
-            SQLiteDataContext.SQLitePath = config.SQLitePath;
+                // SQLite用のファイルパス
+                SQLiteDataContext.SQLitePath = config.SQLitePath;
+                // サービスの作成
+                Listener listener = new Listener(config.HostName, config.Port);
 
-            // サービスの作成
-            Listener listener = new Listener(config.HostName, config.Port);
+                // ログ出力
+                Console.WriteLine(string.Format("SQLitePath={0}", config.SQLitePath));
+                Console.WriteLine(string.Format("Port={0}", config.Port));
 
-            // サービスのスタート
-            listener.Start();
+                // サービスのスタート
+                listener.Start();
 
-            Console.WriteLine("行先ボードの通信処理サービスを開始しました。");
-            Console.ReadLine();
+                Console.WriteLine("行先ボードサービスを開始します。");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e.Message);
+                Console.WriteLine(e.Message);
+            }
         }
 
     }
